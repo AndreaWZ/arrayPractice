2e859d22175c2a5aa15e094d14067451d45cf4a5
Andrea Wu <lixianhyun@gmail.com>
2020-11-11 18:50:19 -0800

    finding an specific comment through 'id' by using Array.prototype.find()

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ const comments = [
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
+//=================================================================
 // Some and Every Checks
-//=====================================================================
+// ---------------------------------------------------------------
 // Array.prototype.some() // is at least one person 19 or older?
 const date = new Date();
 const currentYear = date.getFullYear();
@@ -29,14 +30,22 @@ function checkAges(age) {
 
 console.log(people.some(checkAges));
 
-//======================================================================
+// ---------------------------------------------------------------
 // Array.prototype.every() // is everyone 19 or older?
 console.log(people.every(checkAges));
 
+//=============================================================================
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+function findComment(arr) {
+  return arr.id = 823423;
+}
 
+console.log(people.find(findComment));
+
+
+//======================================================================
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
