441305f6a0cec5c56788f216a4b5d1b6198b95ed
Andrea Wu <lixianhyun@gmail.com>
2020-11-11 19:10:33 -0800

    find an specific comment and delete it by using Array.prototype.findIndex() and splice()

--- main.js
+++ main.js
@@ -39,14 +39,19 @@ console.log(people.every(checkAges));
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 function findComment(arr) {
-  return arr.id = 823423;
+  return arr.id === 823423;
 }
 
-console.log(people.find(findComment));
-
+console.log(comments.find(findComment));
 
 //======================================================================
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+// for(let i = 0; i < comments.length; i ++){
+//   console.log(comments[i].splice(comments.findIndex(findComment), 1));
+// }
+
+console.log(comments.splice(comments.findIndex(findComment), 1));
+console.log(comments);
 
